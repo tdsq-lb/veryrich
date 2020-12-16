5da264423df5603e5145930d631282ca40c65da9
libiao <578556500@qq.com>
2020-12-16 21:08:34 +0800

    1

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const {
+    override,
+    setWebpackPublicPath,
+} = require('customize-cra')
+
+module.exports = override(
+    // 加上这一行，假设打包后的路径为 /console/....
+    setWebpackPublicPath('/build')
+);
